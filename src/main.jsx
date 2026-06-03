62d467ad448d503689dd37a41af92456587295c1
fwdmukesh <fwdmukesh@gmail.com>
2026-06-03 20:32:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Register PWA service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {
+      // Silent fail - PWA is optional
+    })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
